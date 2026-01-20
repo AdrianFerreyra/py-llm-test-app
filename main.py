a04713b57336f38624131f24051b4f472efe4d19
Luis Adrián Ferreyra <ferreyra.luis.adrian@gmail.com>
2026-01-20 21:09:43 +0000

    Read API keys from .env

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 def main():
+    openai_key = os.getenv("OPENAI_API_KEY")
+
+
     print("Hello from py-llm-test-app!")
+    print(f"OpenAI API Key: {openai_key}")
+
 
 
 if __name__ == "__main__":
