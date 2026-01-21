c8abb4a99c6b06a8eefd6c53d4a198c70cd53327
Luis Adrián Ferreyra <ferreyra.luis.adrian@gmail.com>
2026-01-21 06:11:19 +0000

    Hardcoding available tools in OpenAILLMAdapter

--- src/infra/adapters/openai_llm_adapter.py
+++ src/infra/adapters/openai_llm_adapter.py
@@ -5,6 +5,26 @@ from src.application.ports import LLMPort
 
 
 class OpenAILLMAdapter(LLMPort):
+    available_tools = [
+        {
+            "type": "function",
+            "name": "get_current_weather",
+            "description": "Get the current weather in a given location",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "location": {
+                        "type": "string",
+                        "description": "City name, e.g. London"
+                    },
+                },
+                "required": ["location"],
+                "additionalProperties": False
+            },
+            "strict": True
+        }
+    ]
+        
     def __init__(self, api_key: str):
         self.client = OpenAI(api_key=api_key)
 
@@ -15,5 +35,6 @@ class OpenAILLMAdapter(LLMPort):
         response = self.client.responses.create(
             model="gpt-4.1",
             input=messages,
+            tools=self.tools,
         )
         return LLMMessageResponseDTO(message=response.output_text)
